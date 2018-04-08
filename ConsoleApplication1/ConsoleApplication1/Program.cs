762f70e6baa84d4501d739fe1a2254f6f1cf404b
GaoCheng19901208 <384699848@qq.com>
2018-04-08 15:26:27 +0800

    cluster commit

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -6,7 +6,7 @@ using System.Text;
 namespace ConsoleApplication1
 {
     /// <summary>
-    /// ShortestPathByDFS 20180402
+    /// ShortestPathByDFS 20180407 GaoCheng
     /// </summary>
     class Program
     {
